734d889f605ba8bf06daf44b836abce0b50bada9
mennarihan5 <menna1986@live.com>
2024-10-05 23:53:43 +0300

    finalizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors: {
         mediumGrey: "#545454",
         lightBlack: "#1F1F1F",
-        lightGrey: "#DADADA"
+        lightGrey: "#DADADA",
+        lightGrey2: "#8C8C8C",
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'] 
